56a4e5be9dedb30c78f0c29150a6988a96dac541
Lynderly Orphan <lorphan@mi-l5.msk.21-school.ru>
2021-09-21 16:36:28 +0300

    final fix for red cross buttin and escape. Starting Mandelbrot set

--- includes/fractol.h
+++ includes/fractol.h
@@ -6,20 +6,29 @@
 /*   By: lorphan <lorphan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/16 17:15:06 by lorphan           #+#    #+#             */
-/*   Updated: 2021/09/21 16:10:29 by lorphan          ###   ########.fr       */
+/*   Updated: 2021/09/21 16:35:39 by lorphan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FRACTOL_H
 # define FRACTOL_H
 
+/* Window size */
+
 # define WIN_WIDTH 1920
 # define WIN_HEIGHT 1080
+
+/* Keycodes */
+
 # define KEY_ESC 53
+
+/* Headers */
 
 # include <mlx.h>
 # include <stdlib.h>
 # include <stdio.h>
+
+/* Structures */
 
 typedef struct s_image
 {
@@ -36,6 +45,8 @@ typedef struct s_win
 	void	*win;
 	t_image	img;
 }	t_win;
+
+/* Function prototypes */
 
 int		red_cross_pressed(t_win *vars);
 int		esc_button_pressed(int keycode, t_win *vars);
